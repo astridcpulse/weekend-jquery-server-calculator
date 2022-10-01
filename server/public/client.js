f604605d5d55fa5e542ce512d232db8e1218e42c
Astrid Pulse <astridcpulse@gmail.com>
2022-10-01 09:18:42 -0500

    create functions or each button, store items in global client side object

--- server/public/client.js
+++ server/public/client.js
@@ -2,22 +2,51 @@ console.log('in client');
 
 $(document).ready(onReady);
 
+let opNumObject = {
+    // firstNum: 
+    // secondNum: 
+    // op:
+};
+
 function onReady (){
 
-    console.log("ready girl!!")
+    console.log("ready with NODEMON girl!!")
 
-    // $('#firstNumber').on('submit', )//some function to store number
-    // $('#secondNumber').on('submit', )//some function to second number
-    $('#plusBtn').on('click', )//store this operator
-    $('#minusBtn').on('click', )//store this operator
-    $('#multBtn').on('click', )//store this operator
-    $('#divBtn').on('click', )//store this operator
+    $('#firstNumber').on('submit', numAdder)//some function to store number
+    $('#secondNumber').on('submit', numAdder)//some function to second number
+    $('#plusBtn').on('click', opPlusAdder)//store this operator
+    $('#minusBtn').on('click', opMinusAdder)//store this operator
+    $('#multBtn').on('click', opMultAdder)//store this operator
+    $('#divBtn').on('click', opDivAdder)//store this operator
     $('#numberFields').on('equalBtn', equalFunction)//store this operator
 
 }
+console.log(opNumObject);
 
-function equalFunction(){
+function equalFunction(evt){
+    evt.preventDefault();
 
+}
 
+function numAdder(evt){
+    evt.preventDefault();
+    
+    console.log('opNumAdder', opNumObject);
+}
+
+function opPlusAdder(evt){
+    evt.preventDefault();
+    opNumObject.op = '+';
+}
+
+function opMinusAdder(){
+
+}
+
+function opDivAdder(){
+
+}
+
+function opMultAdder(){
 
 }
