707d09603e98201f4f5be8a88fff3cfc1a1e6e19
Astrid Pulse <astridcpulse@gmail.com>
2022-10-01 10:04:33 -0500

    create equalFunction POST

--- server/server.js
+++ server/server.js
@@ -1,11 +1,22 @@
+//server setup--------
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const PORT = 3000;
 const app = express ();
 
 app.use(express.static('./server/public'));
 app.use(bodyParser.urlencoded({ extended: true}));
 
-app.listen(3000, () =>{
+app.listen(PORT, () =>{
     console.log('we are schlive!!');
 });
+//-----------------------
+
+let calcHistory = [];
+
+
+app.post('/calculation', (req, res) =>{
+    calcHistory.push(req.body);
+    res.sendStatus(200)
+    console.log(calcHistory); //test 
+})
