6cb34c26ba551e721233a744be86d574411f1e29
Astrid Pulse <astridcpulse@gmail.com>
2022-10-01 14:59:54 -0500

    server calculation completed

--- server/server.js
+++ server/server.js
@@ -16,11 +16,11 @@ let calcHistory = [];
 
 
 app.post('/calculation', (req, res) =>{
-    calcHistory.push(req.body);
+    // calcHistory.push(req.body);
 
     serverCalc(req.body);
     res.sendStatus(200)
-    console.log(calcHistory); //test 
+    
 
 })
 
@@ -28,5 +28,38 @@ app.post('/calculation', (req, res) =>{
 
 // do the calculation and store it
 function serverCalc(object){
-    console.log(Number(object.firstNum));
+    // create temp object
+    let fullCalcChar = {
+        // firstServNum:
+        // secondServNum:
+        // servOp:
+        // servTot:
+    };
+
+    // put client-sent info into object
+    fullCalcChar.firstServNum = object.firstNum;
+    fullCalcChar.secondServNum = object.secondNum;
+    
+    // calculating servTot and assigning servOp depending on operator selected on client side
+    switch(object.op) {
+        case '+':
+            fullCalcChar.servTot = Number(object.firstNum) + Number(object.secondNum);
+            fullCalcChar.servOp = '+';
+            break;
+        case '-':
+            fullCalcChar.servTot= Number(object.firstNum) - Number(object.secondNum);
+            fullCalcChar.servOp = '-';
+            break;
+        case '/':
+            fullCalcChar.servTot = Number(object.firstNum) / Number(object.secondNum);
+            fullCalcChar.servOp = '/';
+            break;
+        case '*':
+            fullCalcChar.servTot = Number(object.firstNum) * Number(object.secondNum);
+            fullCalcChar.servOp = '*';
+            break;
+    }
+    // push object to global array calcHistory
+    calcHistory.push(fullCalcChar);
+    console.log(calcHistory); // test
 }
