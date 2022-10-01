6aa342271d5bb592f96c55e06c7a73b52cd1ec97
Astrid Pulse <astridcpulse@gmail.com>
2022-10-01 09:45:19 -0500

    event handler functions complete and store inputs to object

--- server/public/client.js
+++ server/public/client.js
@@ -12,26 +12,32 @@ function onReady (){
 
     console.log("ready with NODEMON girl!!")
 
-    $('#firstNumber').on('submit', numAdder)//some function to store number
-    $('#secondNumber').on('submit', numAdder)//some function to second number
     $('#plusBtn').on('click', opPlusAdder)//store this operator
     $('#minusBtn').on('click', opMinusAdder)//store this operator
     $('#multBtn').on('click', opMultAdder)//store this operator
     $('#divBtn').on('click', opDivAdder)//store this operator
-    $('#numberFields').on('equalBtn', equalFunction)//store this operator
+    $('#numberFields').on('click', '#equalBtn', equalFunction)//store this operator
 
 }
+
+//test: should be empty object
 console.log(opNumObject);
 
 function equalFunction(evt){
     evt.preventDefault();
-
+    opNumObject.firstNum = $('#firstNumber').val(); //value of first number field in calculator
+    opNumObject.secondNum = $('#secondNumber').val(); // value of second number field in calculator
+    
+    //tests: object should be fully populated with nums and op
+    console.log('equal button working');
+    console.log(opNumObject);
 }
 
 function numAdder(evt){
     evt.preventDefault();
-    
-    console.log('opNumAdder', opNumObject);
+    // opNumObject.firstNum = $('#firstNumber').val();
+    // opNumObject.secondNum = $('#secondNumber').val();
+
 }
 
 function opPlusAdder(evt){
@@ -39,14 +45,17 @@ function opPlusAdder(evt){
     opNumObject.op = '+';
 }
 
-function opMinusAdder(){
-
+function opMinusAdder(evt){
+    evt.preventDefault();
+    opNumObject.op = '-';
 }
 
-function opDivAdder(){
-
+function opDivAdder(evt){
+    evt.preventDefault();
+    opNumObject.op = '/';
 }
 
-function opMultAdder(){
-
+function opMultAdder(evt){
+    evt.preventDefault();
+    opNumObject.op = '*';
 }
